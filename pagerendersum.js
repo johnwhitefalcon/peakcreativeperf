d633854c6c2f01e2afe50ee9314a1d7beedb76f8
johnwhitefalcon <87903601+johnwhitefalcon@users.noreply.github.com>
2022-10-15 16:02:18 +1100

    Add files via upload

--- pagerendersum.js
+++ pagerendersum.js
@@ -23,23 +23,51 @@ fetchdashboarddata();
 
 
 
-const isObject = function(val){
-    if(val===null){
-      return false;
-    }
-return(typeof val === 'object')
 
-};
+  
 
-const objProps = function(obj){
-  for(var val in obj){
-    if(isObject(obj[val])){
-      objProps(obj[val])
-    } else {console.log(val, obj[val])}
-  }
+
+var hold2 = comments.map(function(item){
+ return item
+  
+})
+
+
+var store = []
+
+var hold3 = comments.map(function(it){
+
+   store.push(it.data.Q1, it.data.Q2, it.data.Q3, it.data.Q4, it.data.Q5, it.data.Q6, it.data.Q7, it.data.Q8, it.data.Q9, it.data.Q10)
+
+  
+  
+})
+
+
+
+var store1 = [];
+var store2 = [];
+
+for(var i = 0; i<store.length; i = 0){ 
+var cut = store.splice(0, 10)
+
+store1.push(cut)
+
+var flat = store1.flat()
+var test = flat.map(function(item){
+  return Number(item)
+})
+
+var full = test.reduce(function(a,b){return a+b},0)
+store2.push(full)
+
+var store1 = []
 }
+  
 
-objProps(comments);
+
+
+
 
 
 
@@ -49,7 +77,8 @@ objProps(comments);
 return (
 
 <div className="font-bold text-9xl items-center flex ml-[20rem] mt-[10rem]">
-  {}
+  {store2}
+  
 
 
 </div>
